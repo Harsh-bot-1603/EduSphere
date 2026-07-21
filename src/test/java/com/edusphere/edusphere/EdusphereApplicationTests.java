66acb98a78a31b5b3b5a93f11b231ee04774e182
Harsh-bot-1603 <harshdpandey1603@gmail.com>
2026-07-21 17:46:11 +0530

    Initial Structure of project

---
+++ src/test/java/com/edusphere/edusphere/EdusphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusphere.edusphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
